58036c725a65d50f0c90a6c37fc58de9b079fcd5
Alfonso Gomez <alfonso.gomez@struck.com>
2021-01-05 02:03:48 -0600

    ✏️ Updated and added new views

--- src/services/voxa/views/views.ts
+++ src/services/voxa/views/views.ts
@@ -1,24 +1,30 @@
 const views = {
   welcome: {
-    say: "Hola, bienvenido a cuentos de Pancho Madrigal."
+    say: "Hola, bienvenido a cuentos de Pancho Madrigal. Para pedirle a pancho que cuente un cuento puedes decir: Alexa, dile a Pancho Madrigal que me cuente un cuento."
   },
   askForStory: {
     say: "¿Que cuento de pancho madrigal te gustaria escuchar?"
   },
   search: {
-    say: "Buscando el cuento: {currentStory}.<break time='3s'/>"
+    say: "Buscando el cuento: {currentStory}.<break time=\"3s\"/>"
   },
   storyFoundState: {
     say: "Reproduciendo {currentStory}."
   },
   storyNotFound: {
-    say: "No encontre un cuento relacionado a {currentStory}. ¿Te gustaria escuchar un cuento popular de pancho?"
+    say: "No encontre un cuento relacionado. ¿Te gustaria escuchar un cuento popular de pancho?"
   },
   farewell: {
     say: "Gracias por escuchar los cuentos. Como diría Pancho: Auténtico amigo. Ahi nos vemos Filiberto!"
   },
   error: {
     say: "Hubo un error al inicial la skill. Contactate con el desarrollador."
+  },
+  help: {
+    say: "Para escuchar los cuentos puedes decir: Alexa, dile a Pancho Madrigal que haga lo suyo, o Alexa, dile a Pancho Madrigal que nos cuente un cuento."
+  },
+  developerNote: {
+    say: "Nota del desarrollador. Muchas gracias por usar la habilidad de cuentos de Pancho. Esta habilidad fue creada en memoria de dos grandes fanaticos de estas entretenidas historias, y con la unica motivacion de entretenerlos a ustedes tanto como los cuentos de Pancho entretenian a mis queridos abuelos. En paz descansen, Emilio Alfonso Selva Maldonado y Ana del Socorro Melendez Quiroz."
   }
 }
 
