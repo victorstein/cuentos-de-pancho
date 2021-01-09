464a9345e57aa5b1aeb138edbae51900bfecc0b2
Alfonso Gomez <alfonso.gomez@struck.com>
2021-01-08 21:44:39 -0600

    ✨ Added error handling

--- src/services/voxa/index.ts
+++ src/services/voxa/index.ts
@@ -1,13 +1,11 @@
-import { AlexaPlatform, VoxaApp, VoxaEvent } from 'voxa'
+import { AlexaPlatform, IVoxaReply, VoxaApp, VoxaEvent } from 'voxa'
 import views from './views'
 import variables from './variables'
 import VoxaStates from './states'
 import { Service } from 'typedi'
 import { Listeners, ViewType, State, Intent, Middleware } from './types'
 import Model from './model'
-import { Transaction } from '@sentry/tracing'
 import * as Sentry from '@sentry/node'
-import '@sentry/tracing'
 
 @Service()
 export class Voxa {
@@ -15,7 +13,6 @@ export class Voxa {
   views: ViewType
   variables: any
   state: Listeners
-  transaction: Transaction | void
 
   constructor () {
     this.views = views
@@ -29,23 +26,16 @@ export class Voxa {
     stateType.forEach(({ name, handler, middleware }: State | Intent) => {
       this.app[listenerType](name, async (voxaEvent: VoxaEvent) => {
         try {
-          // Create a transaction
-          this.transaction = Sentry.startTransaction({ name, data: voxaEvent }) as Transaction
-
           // Run middlewares
-          if (middleware) { this.transaction = await middleware(name, voxaEvent) }
-          else if (globalMiddleware) { this.transaction = await globalMiddleware(name, voxaEvent) }
+          if (middleware) { await middleware(name, voxaEvent)  }
+          else if (globalMiddleware) { await globalMiddleware(name, voxaEvent) }
 
           // Handle the voxa request
           const response = await handler(voxaEvent)
-
-          // Finish the transaction
-          if (this.transaction) this.transaction.finish()
           
           //Send the response back
           return response
         } catch (e) {
-          if (this.transaction) this.transaction.finish()
           throw new Error(e)
         }
       })
@@ -56,6 +46,20 @@ export class Voxa {
     // Add states And Intent Listeners
     this.parseListeners(this.state.intents, 'onIntent', this.state.middleware)
     this.parseListeners(this.state.states, 'onState', this.state.middleware)
+
+    // Set up onError handler
+    this.app.onError(async function errorHandler(voxaEvent: VoxaEvent, err: Error, reply: IVoxaReply) {
+      try {
+        Sentry.captureException(err)
+        const statement = await voxaEvent.renderer.renderPath("error.say", voxaEvent);
+        reply.clear();
+        reply.addStatement(statement, false);
+        reply.terminate();
+        return reply;
+      } catch (e) {
+        throw new Error(e)
+      }
+    })
   }
 
   getAlexaSkill (): AlexaPlatform {
