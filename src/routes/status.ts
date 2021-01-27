f26618d72f75cb4b11b002bacef28d21d50c399e
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-01-26 21:01:38 -0600

    ♻️ Refactored routes to use decorators

--- src/routes/status.ts
+++ src/routes/status.ts
@@ -1,15 +1,11 @@
+import { Route, Get } from "decorators/express";
 import { Request, Response } from "express";
 import { Service } from "typedi";
-import Endpoint from "./endpoint";
 
 @Service()
-export default class Status extends Endpoint {
-  constructor () {
-    super()
-    this.endpoint = `/status`
-    this.router.get('/', this.get)
-  }
-
+@Route('/status')
+export default class Status {
+  @Get()
   get(_: Request, res: Response) {
     return res.json({ status: 'ok' })
   }
